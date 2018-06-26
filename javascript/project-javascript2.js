add245d595b19179c8fdedc47611dddf8b183b90
z110x119c911 <39883684+z110x119c911@users.noreply.github.com>
2018-06-26 23:04:41 +0800

    Add files via upload

---
+++ javascript/project-javascript2.js
@@ -0,0 +1,107 @@
+function over(element){
+		element.style.color="red";
+	}
+	function out(element){
+		element.style.color="black";
+	}
+	function clk(){
+		clk.h
+	}
+/*change skin*/
+	function skin1()
+	{
+		var sk=document.getElementById('11')
+		sk.href='num1.css';
+	}
+
+	function skin2()
+	{
+		var sk=document.getElementById('11')
+		sk.href='css-js3.css';
+	}	
+
+/*select_left_click*/
+
+	function over_left(element)
+	{
+		element.style.color="blue";
+	}
+
+	function out_left(element)
+	{
+		element.style.color="black";
+	}
+
+	function a1()
+	{
+		var a1 = document.getElementById('test1');
+		var b1 = document.getElementById('test2');
+		var c1 = document.getElementById('test3');
+		var d1 = document.getElementById('test4');
+		var e1 = document.getElementById('test5');
+		a1.style.display = "block";
+		b1.style.display = "none";
+		c1.style.display = "none";
+		d1.style.display = "none";
+		e1.style.display = "none";
+	}
+	function a2()
+	{
+		var a2 = document.getElementById('test1');
+		var b2 = document.getElementById('test2');
+		var c2 = document.getElementById('test3');
+		var d2 = document.getElementById('test4');
+		var e2 = document.getElementById('test5');
+		a2.style.display = "none";
+		b2.style.display = "block";
+		c2.style.display = "none";
+		d2.style.display = "none";
+		e2.style.display = "none";
+	}
+	function a3()
+	{
+		var a3 = document.getElementById('test1');
+		var b3 = document.getElementById('test2');
+		var c3 = document.getElementById('test3');
+		var d3 = document.getElementById('test4');
+		var e3 = document.getElementById('test5');
+		a3.style.display = "none";
+		b3.style.display = "none";
+		c3.style.display = "block";
+		d3.style.display = "none";
+		e3.style.display = "none";
+	}
+	function a4()
+	{
+		var a4 = document.getElementById('test1');
+		var b4 = document.getElementById('test2');
+		var c4 = document.getElementById('test3');
+		var d4 = document.getElementById('test4');
+		var e4 = document.getElementById('test5');
+		a4.style.display = "none";
+		b4.style.display = "none";
+		c4.style.display = "none";
+		d4.style.display = "block";
+		e4.style.display = "none";
+	}
+	function a5()
+	{
+		var a5 = document.getElementById('test1');
+		var b5 = document.getElementById('test2');
+		var c5 = document.getElementById('test3');
+		var d5 = document.getElementById('test4');
+		var e5 = document.getElementById('test5');
+		a5.style.display = "none";
+		b5.style.display = "none";
+		c5.style.display = "none";
+		d5.style.display = "none";
+		e5.style.display = "block";
+	}
+
+	/*function hide()
+	{
+		document.getElementById('test2').style.visibility  = "hidden"
+		document.getElementById('test3').style.visibility = "hidden"
+		document.getElementById('test4').style.visibility = "hidden"
+		document.getElementById('test5').style.visibility = "hidden"
+	}*/
